bee8a75171389b0c8de04538c7b2fb2dc092c1c9
Begum S. Tuyen <begumtuyen@gmail.com>
2019-02-05 01:06:12 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array){
+  for(var i=0; i<25; i++){
+    if(i<=1){
+      array.push(`I am ` + i + ` strange loop.`); 
+    } else {
+      array.push(`I am ` + i + ` strange loops.`); 
+    }
+  }
+  return array;
+}
+
+function whileLoop(){
+  while(n>0){
+    console.log(n);
+    n--;
+  }
+  return "done";
+}
+
+function doWhileLoop(array){
+  function incrementVariable(i) {
+  i = i + 1;
+  do {
+  console.log("I run once regardless.");
+  array.pop();
+} while (incrementVariable() < i && array.length > 0);
+}
+}
+
